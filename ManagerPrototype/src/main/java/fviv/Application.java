4db8231f055556bba170a175a1daa90856d6aca8
Hendric <Hendric.Eckelt@mailbox.tu-dresden.de>
2014-11-19 19:27:19 +0100

    ManagerPrototype added - no functions implemented yet

---
+++ ManagerPrototype/src/main/java/fviv/Application.java
@@ -0,0 +1,15 @@
+package fviv;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+	public static void main(String[] args){
+		SpringApplication.run(Application.class, args);
+	}
+}
