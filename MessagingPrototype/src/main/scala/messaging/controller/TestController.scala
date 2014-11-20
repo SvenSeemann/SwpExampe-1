dba98089e9658fc37be969c7b755d0803673d229
JustusAdam <development@justusadam.com>
2014-11-20 12:59:18 +0100

    fixing test interface

--- MessagingPrototype/src/main/scala/messaging/controller/TestController.scala
+++ MessagingPrototype/src/main/scala/messaging/controller/TestController.scala
@@ -6,6 +6,8 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.{PathVariable, RequestMapping}
 import people.People
 
+import scala.collection.JavaConverters
+
 /**
  * Created by justusadam on 19/11/14.
  */
@@ -13,6 +15,12 @@ import people.People
 @Controller
 @Autowired
 class TestController {
+
+  @RequestMapping(Array("/choose"))
+  def page(model:Model) = {
+    model.addAttribute("people", JavaConverters.asJavaIterableConverter(People.people.values).asJava)
+    "overview"
+  }
 
 
   @RequestMapping(Array("/choose/{id}"))
--- MessagingPrototype/src/main/scala/people/Employee.scala
+++ MessagingPrototype/src/main/scala/people/Employee.scala
@@ -1,10 +1,17 @@
 package people
 
+import javax.persistence.Entity
+
 /**
  * Created by justusadam on 19/11/14.
  */
+@Entity
 class Employee(private val _id:Int, private val _name:String) extends Person{
 
   def name = _name
   def id = _id
+
+  def getId = _id
+
+  def getName = _name
 }
