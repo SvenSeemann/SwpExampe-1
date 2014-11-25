18801fa2652882b1a994d36e6d6a5ac9767e7707
Fallik <niklas.fallik@mail.zih.tu-dresden.de>
2014-11-25 09:54:00 +0100

    Some Exceptions ...141 more

--- CateringPrototype/src/main/java/catering/CateringPrototype.java
+++ CateringPrototype/src/main/java/catering/CateringPrototype.java
@@ -26,8 +26,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 @ComponentScan
 public class CateringPrototype {
 
-	
-	
     public static void main(String[] args) {
         SpringApplication.run(CateringPrototype.class, args);
     }
@@ -35,12 +33,6 @@ public class CateringPrototype {
     @Configuration
 	static class CateringPrototypeWebConfiguration extends SalespointWebConfiguration {
 
-		/**
-		 * We configure {@code /login} to be directly routed to the {@code login} template without any messaging.controller
-		 * interaction.
-		 * 
-		 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry)
-		 */
 		@Override
 		public void addViewControllers(ViewControllerRegistry registry) {
 			registry.addViewController("/login").setViewName("login");
@@ -56,27 +48,18 @@ public class CateringPrototype {
 
 		return characterEncodingFilter;
 	} 
-    
-    
-   Order order;
+
    
-    
-    
+   //Order order;
+   
     @Configuration
 	@EnableGlobalMethodSecurity(prePostEnabled = true)
 	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
 
-		/**
-		 * Disabling Spring Security's CSRF support as we do not implement pre-flight request handling for the sake of
-		 * simplicity. Setting up basic security and defining login and logout options.
-		 * 
-		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
-		 */
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-
+			
 			http.csrf().disable();
-
 			http.authorizeRequests().antMatchers("/**").permitAll().and().//
 					formLogin().loginPage("/login").loginProcessingUrl("/login").and(). //
 					logout().logoutUrl("/logout").logoutSuccessUrl("/");
--- CateringPrototype/src/main/java/catering/OrderController.java
+++ CateringPrototype/src/main/java/catering/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,114 +22,145 @@ public class OrderController {
 	
 	@Autowired				//constructor is not a valid setter method or instance variable
 	private Order order;
-	private Menu menu1, menu2, menu3, menu4, menu5, menu6, menu7, menu8;
+	private String mode = "";
+	//private final OrderedDrinksRepository orderedDrinksRepository;
+	//private final OrderedMealsRepository orderedMealsRepository;
 	
-	//OrderController() {}
-	
-	public OrderController() {
-		order = new Order();
-		Assert.notNull(order, "Order must not be null!");
-		//this.order = order;
+	@Autowired
+	public OrderController(OrderedMealsRepository orderedMealsRepository, OrderedDrinksRepository orderedDrinksRepository) {
 		
-		menu1 = new Menu(0, "Pommes Frites", 2.50);
-		menu2 = new Menu(1, "Pommes Spezial", 3.50);
-		menu3 = new Menu(2, "Bratwurst", 2.00);
-		menu4 = new Menu(3, "Currywurst", 3.00);
-		menu5 = new Menu(4, "Currywurst Pommes", 4.50);
-		menu6 = new Menu(5, "Stück Pizza", 2.50);
-		menu7 = new Menu(6, "Vanilleeis", 1.00);
-		menu8 = new Menu(7, "Schokoeis", 1.00);
+		
+		Assert.notNull(order, "Order must not be null!");
+		//this.orderedMealsRepository = orderedMealsRepository;		// kann vllt weg
+		//this.orderedDrinksRepository = orderedDrinksRepository;
+		order = new Order(orderedMealsRepository, orderedDrinksRepository);
 	}
+	
+	//@Deprecated
+	//protected OrderController() {}
 	
 	// --- --- --- --- --- --- ModelAttributes --- --- --- --- --- --- \\
 	
-	@ModelAttribute("output0")
-	public String output0() {
-		if (order.hasNoMenus()) return "There were no menus ordered."; else
-		return order.getOutput(0);
-	}
-	
-	@ModelAttribute("output1")
-	public String output1() {
-		return order.getOutput(1);
-	}
-	
-	@ModelAttribute("output2")
-	public String output2() {
-		return order.getOutput(2);
-	}
-	
-	@ModelAttribute("output3")
-	public String output3() {
-		return order.getOutput(3);
-	}
-	
-	@ModelAttribute("output4")
-	public String output4() {
-		return order.getOutput(4);
-	}
-	
-	@ModelAttribute("output5")
-	public String output5() {
-		return order.getOutput(5);
-	}
-	
-	@ModelAttribute("output6")
-	public String output6() {
-		return order.getOutput(6);
-	}
-	
-	@ModelAttribute("output7")
-	public String output7() {
-		return order.getOutput(7);
+	@ModelAttribute("ordermode")
+	public String ordermode() {
+		return mode;
 	}
 	
 	// --- --- --- --- --- --- RequestMapping --- --- --- --- --- --- \\
 	
 	@RequestMapping("/")				
-	public String index() {
+	public String index(ModelMap modelMap) {
+		modelMap.addAttribute("meals", order.getOrderedMeals().findAll());
+		modelMap.addAttribute("drinks", order.getOrderedDrinks().findAll());
 		return "/order";
 	}
 	
+	@RequestMapping("/cancel")
+	public String cancel() {
+		order.cancel();
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/drinks")
+	public String drinks() {
+		mode = "drinks";
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/meals")
+	public String meals() {
+		mode = "meals";
+		return "redirect:/";
+	}
+	
 	@RequestMapping("/menu1")
-	public String menu1(Model model) {			//@RequestParam("menu") Menu menu
-		order.addMenu(menu1);
+	public String menu1() {
+		Meal meal = new Meal("Pommes Frites", 2.50f);
+		order.addMealToRepository(meal);
 		return "redirect:/";
 	}
 	
 	@RequestMapping("/menu2")
-	public String menu2(Model model) {
-		order.addMenu(menu2);
+	public String menu2() {
+		Meal meal = new Meal("Pommes Spezial", 3.50f);
+		order.addMealToRepository(meal);
 		return "redirect:/";
 	}
+	
 	@RequestMapping("/menu3")
-	public String menu3(Model model) {
-		order.addMenu(menu3);
+	public String menu3() {
+		Meal meal = new Meal("Bratwurst", 2.00f);
+		order.addMealToRepository(meal);
 		return "redirect:/";
 	}
+	
 	@RequestMapping("/menu4")
-	public String menu4(Model model) {
-		order.addMenu(menu4);
+	public String menu4() {
+		Meal meal = new Meal("Currywurst", 3.00f);
+		order.addMealToRepository(meal);
 		return "redirect:/";
 	}
+	
 	@RequestMapping("/menu5")
-	public String menu5(Model model) {
-		order.addMenu(menu5);
+	public String menu5() {
+		Meal meal = new Meal("Currywurst Pommes", 4.50f);
+		order.addMealToRepository(meal);
 		return "redirect:/";
 	}
+	
 	@RequestMapping("/menu6")
-	public String menu6(Model model) {
-		order.addMenu(menu6);
+	public String menu6() {
+		Meal meal = new Meal("Stück Pizza", 2.50f);
+		order.addMealToRepository(meal);
 		return "redirect:/";
 	}
+	
 	@RequestMapping("/menu7")
-	public String menu7(Model model) {
-		order.addMenu(menu7);
+	public String menu7() {
+		Meal meal = new Meal("Vanilleeis", 1.00f);
+		order.addMealToRepository(meal);
 		return "redirect:/";
 	}
+	
 	@RequestMapping("/menu8")
-	public String menu8(Model model) {
-		order.addMenu(menu8);
+	public String menu8() {
+		Meal meal = new Meal("Schokoeis", 1.00f);
+		order.addMealToRepository(meal);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/menu9")
+	public String menu9() {
+		Drink drink = new Drink("Pils", 2.50f);
+		order.addDrinkToRepository(drink);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/menu10")
+	public String menu10() {
+		Drink drink = new Drink("Alt", 3.00f);
+		order.addDrinkToRepository(drink);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/menu11")
+	public String menu11() {
+		Drink drink = new Drink("Alkoholfrei", 3.00f);
+		order.addDrinkToRepository(drink);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/menu12")
+	public String menu12() {
+		Drink drink = new Drink("Softdrink", 2.20f);
+		order.addDrinkToRepository(drink);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/menu13")
+	public String menu13() {
+		Drink drink = new Drink("Wein", 4.70f);
+		order.addDrinkToRepository(drink);
 		return "redirect:/";
 	}
 	
