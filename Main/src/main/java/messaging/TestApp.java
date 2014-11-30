c936c0c4889de9ba753994ee0a1c3dda4c897480
JustusAdam <development@justusadam.com>
2014-11-30 15:48:10 +0100

    added main project as clone of Messaging

---
+++ Main/src/main/java/messaging/TestApp.java
@@ -0,0 +1,43 @@
+package messaging;
+
+import org.salespointframework.Salespoint;
+import org.salespointframework.SalespointSecurityConfiguration;
+import org.salespointframework.SalespointWebConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+/**
+ * Created by justusadam on 20/11/14.
+ */
+@Configuration
+@EnableAutoConfiguration
+@Import({ SalespointWebConfiguration.class })
+@EntityScan(basePackageClasses = {Salespoint.class, TestApp.class})
+@EnableJpaRepositories(basePackageClasses = {Salespoint.class, TestApp.class})
+@ComponentScan
+public class TestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApp.class, args);
+    }
+
+    @Configuration
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http.csrf().disable();
+
+            http.authorizeRequests().antMatchers("/**", "/resources/**").permitAll().and().formLogin().loginProcessingUrl("/login").and()
+                    .logout().logoutUrl("/logout").logoutSuccessUrl("/");
+        }
+    }
+}
---
+++ Main/src/main/scala/messaging/MessageForm.scala
@@ -0,0 +1,19 @@
+package messaging
+
+import scala.collection.JavaConversions
+
+/**
+ * Created by justusadam on 20/11/14.
+ */
+class MessageForm(val receives:Boolean, val sends:Boolean, val message:Iterable[String]) {
+
+  def getReceives = receives
+
+  def getSends = sends
+
+  def getMessage = JavaConversions.asJavaIterable(message)
+
+  def this(receives:Boolean, sends:Boolean) {
+    this(receives, sends, List[String]())
+  }
+}
