1a77b352ba6679c701adeb0733e76fd691064f25
HendricEckelt <Hendric.Eckelt@mailbox.tu-dresden.de>
2014-12-12 17:34:45 +0100

    manager structure changed and style added

--- src/main/java/fviv/controller/ManagerController.java
+++ src/main/java/fviv/controller/ManagerController.java
@@ -1,5 +1,7 @@
 package fviv.controller;
 
+import java.util.LinkedList;
+
 import javax.validation.Valid;
 
 import org.salespointframework.useraccount.Role;
@@ -22,96 +24,159 @@ import fviv.model.Registration;
 
 @PreAuthorize("hasRole('ROLE_MANAGER')")
 @Controller
-class ManagerFunctionsController{
+public class ManagerController {
+	private String mode = "";
 	private final EmployeeRepository employeeRepository;
 	private final ExpenseRepository expenseRepository;
 	private final UserAccountManager userAccountManager;
-	
+
 	@Autowired
-	public ManagerFunctionsController(EmployeeRepository employeeRepository, ExpenseRepository expenseRepository, UserAccountManager userAccountManager){
+	public ManagerController(EmployeeRepository employeeRepository,
+			ExpenseRepository expenseRepository,
+			UserAccountManager userAccountManager) {
 		this.employeeRepository = employeeRepository;
 		this.expenseRepository = expenseRepository;
 		this.userAccountManager = userAccountManager;
 	}
-	
-	@RequestMapping({"/manager"})
-	public String index(){
-		return "manager";
+
+	@ModelAttribute("managermode")
+	public String managermode() {
+		return mode;
 	}
-	
-	@RequestMapping("/checkEmployees")
-	public String checkEmployees(ModelMap modelMap){
+
+	@RequestMapping({ "/manager" })
+	public String index(ModelMap modelMap) {
+		float salaryTotal = 0, cateringTotal = 0, rentTotal = 0, deposit = 0;
+
+		final Role bossRole = new Role("ROLE_BOSS");
+		final Role managerRole = new Role("ROLE_MANAGER");
+		final Role catererRole = new Role("ROLE_CATERER");
+		final Role employeeRole = new Role("ROLE_EMPLOYEE");
+
+		Iterable<UserAccount> userAccounts = userAccountManager.findAll();
+		LinkedList<UserAccount> bossAccounts = new LinkedList<UserAccount>();
+		LinkedList<UserAccount> managerAccounts = new LinkedList<UserAccount>();
+		LinkedList<UserAccount> catererAccounts = new LinkedList<UserAccount>();
+		LinkedList<UserAccount> employeeAccounts = new LinkedList<UserAccount>();
+
+		// Sort accounts by Role
+		for (UserAccount userAccount : userAccounts) {
+			if (userAccount.hasRole(bossRole))
+				bossAccounts.add(userAccount);
+			if (userAccount.hasRole(managerRole))
+				managerAccounts.add(userAccount);
+			if (userAccount.hasRole(catererRole))
+				catererAccounts.add(userAccount);
+			if (userAccount.hasRole(employeeRole))
+				employeeAccounts.add(userAccount);
+		}
+
+		// Add accounts to modelMap
+		modelMap.addAttribute("bossAccounts", bossAccounts);
+		modelMap.addAttribute("managerAccounts", managerAccounts);
+		modelMap.addAttribute("catererAccounts", catererAccounts);
+		modelMap.addAttribute("employeeAccounts", employeeAccounts);
+
+		// Data needed for registration of new employees
 		modelMap.addAttribute("employeelist", employeeRepository.findAll());
 		modelMap.addAttribute("registration", new Registration());
-		return "checkEmployees";
-	}	
-	
-	@RequestMapping("/newEmployee")
-	public String newEmployee(@ModelAttribute(value="registration") @Valid Registration registration, BindingResult results){
-		//Return to checkEmployees if new employee data are invalid or incomplete
-		if(results.hasErrors())return "redirect:/checkEmployees";
-		
-		final Role employeeRole = new Role("ROLE_EMPLOYEE");
-		
-		//Create useraccount
-		UserAccount employeeAccount = userAccountManager.create(registration.getLastname(), "123", employeeRole);
-		
-		//Create employee
-		Employee employee = new Employee(employeeAccount, registration.getLastname(), registration.getFirstname(), 
-										registration.getEmail(), registration.getPhone());
-		
-		//Add employee and useraccount to the specific repositories
-		userAccountManager.save(employeeAccount);
-		employeeRepository.save(employee);
-		
-		return "redirect:/checkEmployees";
-	}
-	
-	@RequestMapping("/deleteEmployee")
-	public String deleteEmployee(@RequestParam("eId") long eId){
-		if(employeeRepository.findById(eId) == null){
-			return "redirect:/checkEmployees";
-		}
-		
-		Employee deleteThisEmployee = employeeRepository.findById(eId);
-		userAccountManager.disable(deleteThisEmployee.getUserAccount().getId());;
-		employeeRepository.delete(deleteThisEmployee);
-		
-		return "redirect:/checkEmployees";
-	}
-	
-	@RequestMapping("/checkFinances")
-	public String checkFinances(ModelMap modelMap){
-		float salaryTotal = 0, cateringTotal = 0, rentTotal = 0, deposit = 0;
-		modelMap.addAttribute("salary", expenseRepository.findByExpenseType("salary"));
-		modelMap.addAttribute("catering", expenseRepository.findByExpenseType("catering"));
-		modelMap.addAttribute("rent", expenseRepository.findByExpenseType("rent"));
-		
-		for(Expense exp : expenseRepository.findByExpenseType("salary")){
+
+		// Finances
+		modelMap.addAttribute("salary",
+				expenseRepository.findByExpenseType("salary"));
+		modelMap.addAttribute("catering",
+				expenseRepository.findByExpenseType("catering"));
+		modelMap.addAttribute("rent",
+				expenseRepository.findByExpenseType("rent"));
+
+		// Calculate total amounts of each expense type
+		for (Expense exp : expenseRepository.findByExpenseType("salary")) {
 			salaryTotal += exp.getAmount();
 		}
-		
-		for(Expense exp : expenseRepository.findByExpenseType("catering")){
+
+		for (Expense exp : expenseRepository.findByExpenseType("catering")) {
 			cateringTotal += exp.getAmount();
 		}
-		
-		for(Expense exp : expenseRepository.findByExpenseType("rent")){
+
+		for (Expense exp : expenseRepository.findByExpenseType("rent")) {
 			rentTotal += exp.getAmount();
 		}
-		
-		for(Expense exp : expenseRepository.findByExpenseType("deposit")){
+
+		for (Expense exp : expenseRepository.findByExpenseType("deposit")) {
 			deposit += exp.getAmount();
 		}
-		
+
+		// Add deposit and total amounts to modelMap
 		modelMap.addAttribute("deposit", deposit);
 		modelMap.addAttribute("salaryTotal", salaryTotal);
 		modelMap.addAttribute("cateringTotal", cateringTotal);
 		modelMap.addAttribute("rentTotal", rentTotal);
-		return "checkFinances";
+
+		return "manager";
 	}
-	
-	@RequestMapping("/accountAdministration")
-	public String newLogin(ModelMap modelMap){
-		return "accountAdministration";
+
+	@RequestMapping("/Employees")
+	public String employees() {
+		mode = "employees";
+		return "redirect:/manager";
+	}
+
+	@RequestMapping("/newEmployee")
+	public String newEmployee(
+			@ModelAttribute(value = "registration") @Valid Registration registration,
+			BindingResult results) {
+		// Return to checkEmployees if new employee data are invalid or
+		// incomplete
+		if (results.hasErrors())
+			return "redirect:/manager";
+
+		final Role employeeRole = new Role("ROLE_EMPLOYEE");
+
+		// Create useraccount
+		UserAccount employeeAccount = userAccountManager.create(
+				registration.getLastname(), "123", employeeRole);
+
+		// Create employee
+		Employee employee = new Employee(employeeAccount,
+				registration.getLastname(), registration.getFirstname(),
+				registration.getEmail(), registration.getPhone());
+
+		// Add employee and useraccount to the specific repositories
+		userAccountManager.save(employeeAccount);
+		employeeRepository.save(employee);
+
+		return "redirect:/manager";
+	}
+
+	@RequestMapping("/deleteEmployee")
+	public String deleteEmployee(@RequestParam("employeeId") long employeeId) {
+		if (employeeRepository.findById(employeeId) == null) {
+			return "redirect:/manager";
+		}
+
+		Employee deleteThisEmployee = employeeRepository.findById(employeeId);
+		userAccountManager.disable(deleteThisEmployee.getUserAccount().getId());
+		;
+		employeeRepository.delete(deleteThisEmployee);
+
+		return "redirect:/manager";
+	}
+
+	@RequestMapping("/Finances")
+	public String finances() {
+		mode = "finances";
+		return "redirect:/manager";
+	}
+
+	@RequestMapping("/Accountmanagement")
+	public String newLogin() {
+		mode = "accounts";
+		return "redirect:/manager";
+	}
+
+	@RequestMapping("/Terminal")
+	public String terminal() {
+		mode = "terminal";
+		return "redirect:/manager";
 	}
 }
