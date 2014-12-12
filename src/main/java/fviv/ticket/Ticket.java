6efdb675f1f12c7d5aed87269c7af9e219f63b96
Nikolai Kostka <nikolai.kostka@online.de>
2014-12-12 15:44:26 +0100

    arbeiten undso

--- src/main/java/fviv/ticket/Ticket.java
+++ src/main/java/fviv/ticket/Ticket.java
@@ -1,5 +1,42 @@
 package fviv.ticket;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+@Entity
 public class Ticket {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private boolean ticketArt;
+	 private boolean checked;
+	 
+	 @Deprecated
+		protected Ticket(){
+		 
+	 }
+	 
+	 @Autowired
+	public Ticket(boolean ticketArt, boolean checked ){
+		this.ticketArt = ticketArt;
+		this.checked =checked;
+	}
+	public long getId(){
+		return id;
+	}
+	public boolean getticketArt(){
+		return ticketArt; // Eins ist gleich Tagesticket // Null ist gleich 3Tagesticket
+	}
+	public boolean getChecked() {
+		return checked;
+	}
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+	
 
 }
