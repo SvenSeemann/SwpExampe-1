565dbcc3bac727778bad9c5ccbcdfaf71bf06a68
HendricEckelt <Hendric.Eckelt@mailbox.tu-dresden.de>
2014-12-14 20:53:42 +0100

    tests fixed

--- src/test/java/fviv/controller/ManagerControllerTest.java
+++ src/test/java/fviv/controller/ManagerControllerTest.java
@@ -6,6 +6,10 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.ModelMap;
 
 import fviv.AbstractIntegrationTests;
@@ -17,12 +21,20 @@ import fviv.model.EmployeeRepository;
 
 public class ManagerControllerTest extends AbstractIntegrationTests{
 	
+	@Autowired AuthenticationManager am;
 	@Autowired ManagerController controller;
 	@Autowired EmployeeRepository employeeRepository;
+	
+	protected void login(String userName, String password){
+		Authentication auth = new UsernamePasswordAuthenticationToken(userName, password);
+		SecurityContextHolder.getContext().setAuthentication(am.authenticate(auth));
+	}
 	
 	@Test
 	@SuppressWarnings("unchecked")
 	public void managerControllerIntegrationTest(){
+		login("manager", "123");
+		
 		ModelMap modelMap = new ModelMap();
 		String returnedView = controller.index(modelMap);
 		
